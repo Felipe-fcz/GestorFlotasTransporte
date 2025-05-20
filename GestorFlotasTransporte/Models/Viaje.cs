25678727e01bc17c6e929082d4f9da5a497ea5e0
Yohan Suaza <yohan.suaza911@pascualbravo.edu.co>
2025-05-20 01:39:10 -0500

    feat: Se agregó la clase Viaje.cs

---
+++ GestorFlotasTransporte/Models/Viaje.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorFlotasTransporte.Models
+{
+    public class Viaje
+    {
+        public int Id { get; set; }
+        public string MatriculaVehiculo { get; set; }
+        public string IdConductor { get; set; }
+        public string Ruta { get; set; }
+        public DateTime FechaHoraSalida { get; set; }
+        public DateTime FechaHoraLlegada { get; set; }
+        public string Estado { get; set; } // Programado, En curso, Completado, Cancelado
+
+        public Viaje(int id, string matriculaVehiculo, string idConductor, string ruta,
+                    DateTime fechaHoraSalida, DateTime fechaHoraLlegada, string estado)
+        {
+            Id = id;
+            MatriculaVehiculo = matriculaVehiculo;
+            IdConductor = idConductor;
+            Ruta = ruta;
+            FechaHoraSalida = fechaHoraSalida;
+            FechaHoraLlegada = fechaHoraLlegada;
+            Estado = estado;
+        }
+    }
+}
