24d1778c180eddbb0baf4d5351f801e878e0d9c6
Yohan Suaza <yohan.suaza911@pascualbravo.edu.co>
2025-05-20 01:41:36 -0500

    feat: Se agregó la clase Mantenimiento.cs

---
+++ GestorFlotasTransporte/Models/Mantenimiento.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorFlotasTransporte.Models
+{
+    public class Mantenimiento
+    {
+        public int Id { get; set; }
+        public string MatriculaVehiculo { get; set; }
+        public string Tipo { get; set; } // Preventivo, Correctivo
+        public DateTime FechaInicio { get; set; }
+        public DateTime? FechaFin { get; set; }
+        public string Descripcion { get; set; }
+
+        public Mantenimiento(int id, string matriculaVehiculo, string tipo,
+                            DateTime fechaInicio, DateTime? fechaFin, string descripcion)
+        {
+            Id = id;
+            MatriculaVehiculo = matriculaVehiculo;
+            Tipo = tipo;
+            FechaInicio = fechaInicio;
+            FechaFin = fechaFin;
+            Descripcion = descripcion;
+        }
+    }
+}
