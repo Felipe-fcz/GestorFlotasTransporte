43f9e02b1f95166403e0ab6709a33553eb4e7926
Yohan Suaza <yohan.suaza911@pascualbravo.edu.co>
2025-05-20 01:34:10 -0500

    feat: Se agregó la clase Vehiculo.cs

---
+++ GestorFlotasTransporte/Models/Vehiculos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorFlotasTransporte.Models
+{
+    public class Vehiculo
+    {
+        public string Matricula { get; set; }
+        public string Tipo { get; set; }
+        public int Capacidad { get; set; }
+        public string Estado { get; set; } // Disponible, En uso, En mantenimiento
+        public int Kilometraje { get; set; }
+
+        public Vehiculo(string matricula, string tipo, int capacidad, string estado, int kilometraje)
+        {
+            Matricula = matricula;
+            Tipo = tipo;
+            Capacidad = capacidad;
+            Estado = estado;
+            Kilometraje = kilometraje;
+        }
+    }
+}
