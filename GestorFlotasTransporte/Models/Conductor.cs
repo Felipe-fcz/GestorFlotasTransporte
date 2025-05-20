9b23d273179071f02a2ab32b2336168cb5008d0c
Yohan Suaza <yohan.suaza911@pascualbravo.edu.co>
2025-05-20 01:36:57 -0500

    feat: Se agregó la clase Conductor.cs

---
+++ GestorFlotasTransporte/Models/Conductor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorFlotasTransporte.Models
+{
+    public class Conductor
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string Licencia { get; set; }
+        public bool Disponible { get; set; }
+
+        public Conductor(string id, string nombre, string licencia, bool disponible)
+        {
+            Id = id;
+            Nombre = nombre;
+            Licencia = licencia;
+            Disponible = disponible;
+        }
+    }
+}
